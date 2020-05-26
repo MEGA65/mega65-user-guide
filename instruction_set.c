f02bdf48c5aa52fdbd7b17daafb5cba3ad3cf10c
Paul Gardner-Stephen <paul@servalproject.org>
2020-05-26 10:02:40 +0930

    work out which opcodes apply to each instruction.

--- instruction_set.c
+++ instruction_set.c
@@ -19,6 +19,11 @@ int instruction_count=0;
 
 struct opcode opcodes[256];
 
+static int compar_str(const void *a, const void *b)
+{
+  return strcmp(* (char * const *) a, * (char * const *) b);
+}
+
 int main(int argc,char **argv)
 {
   /* Read a list of opcodes. Gather them by instruction, and
@@ -78,12 +83,30 @@ int main(int argc,char **argv)
 
   fprintf(stderr,"%d addressing modes found.\n",mode_count);
   fprintf(stderr,"%d unique instructions found.\n",instruction_count);  
-  
+
+  // Sort instruction names alphabetically
+  fprintf(stderr,"Sorting instructions alphabetically.\n");
+  qsort(&instrs[0],instruction_count,sizeof(char *),compar_str);
+  // Now update the instruction numbers in the array
+  for(int i=0;i<256;i++) {
+    for(int j=0;j<instruction_count;j++) {
+      if (!strcmp(opcodes[i].abbrev,instrs[j])) {
+	opcodes[i].instr_num=j;
+	break;
+      }
+    }
+  }
   
   /* Now generate the instruction tables. 
    */
   for(int i=0;i<instruction_count;i++) {
-    //    fprintf(stderr,"
+    fprintf(stderr,"%s - No description\n",instrs[i]);
+    for(int j=0;j<256;j++) {
+      if (opcodes[j].instr_num==i) {
+	fprintf(stderr,"  $%02x %s\n",
+		j,opcodes[j].mode);
+      }
+    }
   }
   
 }

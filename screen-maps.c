5a53683a3061b65a8b5b9b29cb53a7ef45f9dfb3
Paul Gardner-Stephen <paul@servalproject.org>
2021-04-11 17:16:20 +0930

    draw basic screen layout grid

--- screen-maps.c
+++ screen-maps.c
@@ -11,6 +11,13 @@
 #include <stdio.h>
 #include <hpdf.h>
 
+int columns=40;
+int rows=25;
+int screen_address=0x400;
+int show_addrs=1;
+int show_chars=0;
+int sixteenbit_mode=0;
+
 HPDF_Doc pdf;
 
 void error_handler (HPDF_STATUS error_no, HPDF_STATUS detail_no, void *user_data)
@@ -30,5 +37,28 @@ int main(int argc,char **argv)
     
   /* set compression mode */
   HPDF_SetCompressionMode (pdf, HPDF_COMP_ALL);
+
+  HPDF_Page page_1;
+  
+  page_1 = HPDF_AddPage (pdf);
+
+  HPDF_Page_SetWidth (page_1, 660);
+  HPDF_Page_SetHeight (page_1, 420);
+
+
+
+  for(int x=0;x<=columns;x++) {  
+      HPDF_Page_MoveTo (page_1,10+(x*640/columns),10+0);
+      HPDF_Page_LineTo (page_1,10+(x*640/columns),10+400);
+      HPDF_Page_Stroke (page_1);
+    }
+
+  for(int y=0;y<=rows;y++) {
+    HPDF_Page_MoveTo (page_1,10+0,10+(y*400/rows));
+    HPDF_Page_LineTo (page_1,10+640,10+(y*400/rows));
+    HPDF_Page_Stroke (page_1);
+  }
+  
+  HPDF_SaveToFile (pdf, "test.pdf");
   
 }

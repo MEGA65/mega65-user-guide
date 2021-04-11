7b8d9c38cf583c649593075333de30ef5490ae31
Paul Gardner-Stephen <paul@servalproject.org>
2021-04-11 17:30:00 +0930

    get PDF text drawing working

--- screen-maps.c
+++ screen-maps.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <hpdf.h>
 
+int logic_columns=40;
 int columns=40;
 int rows=25;
 int screen_address=0x400;
@@ -58,6 +59,22 @@ int main(int argc,char **argv)
     HPDF_Page_LineTo (page_1,10+640,10+(y*400/rows));
     HPDF_Page_Stroke (page_1);
   }
+
+  //  const char *font_name = HPDF_LoadTTFontFromFile (pdf, "./fonts/mega40-Regular.ttf", HPDF_TRUE);
+  HPDF_Font font = HPDF_GetFont (pdf, "Helvetica", "CP1250");
+  
+  HPDF_Page_SetFontAndSize(page_1,font,640/columns/2);
+
+  HPDF_Page_BeginText(page_1);
+  
+  for(int y=0;y<rows;y++) {
+    for(int x=0;x<columns;x++) {
+      HPDF_Page_TextOut(page_1,10+1+(x*640/columns),400-(y*400/rows),"1");
+      
+    }
+  }
+
+  HPDF_Page_EndText(page_1);
   
   HPDF_SaveToFile (pdf, "test.pdf");
   

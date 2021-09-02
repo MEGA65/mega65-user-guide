60b38bd595f3f21909cda4092f771d6ba80e7a13
Bit Shifter <Edilbert.Kirk@gmx.de>
2021-09-02 09:07:49 +0200

    add file

---
+++ generate_condensed.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <ctype.h>
+
+FILE *rp;
+FILE *wp;
+
+char Line[60000];
+
+int main(int argc, char *argv[])
+{
+   rp = fopen("appendix-basic65-indexed.tex","r");
+   if (!rp)
+   {
+      fprintf(stderr,"*** Could not open appendix-basic65-indexed.tex\n");
+      exit(1);
+   }
+
+   wp = fopen("appendix-basic65-condensed.tex","w");
+   if (!wp)
+   {
+      fprintf(stderr,"*** Could not open appendix-basic65-condensed.tex\n");
+      exit(1);
+   }
+
+   while (!feof(rp))
+   {
+      fgets(Line,sizeof(Line),rp);
+      if (!strncmp(Line,"\\newpage",8)) continue;
+      if (!strncmp(Line,"\\item [Token:",13)) continue;
+      fputs(Line,wp);
+   }
+
+   return 0;
+}

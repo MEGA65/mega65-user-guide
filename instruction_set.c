737207b4538a34741ee1529fc9888046551cc34e
Oliver Graf <ograf@devilsclaw.de>
2022-06-13 21:12:33 +0200

    fix: cleanup of awkward instruction sorting
    also removed old table generation code

--- instruction_set.c
+++ instruction_set.c
@@ -34,11 +34,16 @@ struct modeinfo {
   unsigned char isQuad;
 };
 
+typedef struct {
+  int instr_num;
+  char *name;
+} instinfo;
+
 int mode_count = 0;
 struct modeinfo modeinfo[MAX_MODES];
 
-char *instrs[256];
 int instruction_count = 0;
+instinfo instruction[256];
 
 #define MAX_OPCODES 256*4
 struct opcode opcodes[MAX_OPCODES];
@@ -48,15 +53,12 @@ int cycle_counts = 0;
 char *cycle_count_list[1024];
 unsigned int cycle_count_list_bytes[1024];
 
-char *instruction_names[256];
-
 // static variables and arrays
 
 //   0     1     2     3     4     5     6
 //   nflag,zflag,iflag,cflag,dflag,vflag,eflag;
 char pflags[7][16];
 
-char *instruction;
 int CPU;
 int extra_cycles;
 size_t len;
@@ -112,8 +114,8 @@ char *Fgets(char *Str, size_t Size, FILE* F) {
   return NULL;
 }
 
-static int compar_str(const void* a, const void* b) {
-  return strcmp(*(char *const*)a, *(char *const*)b);
+static int compar_inst(const void* a, const void* b) {
+  return strcmp(((instinfo *)a)->name, ((instinfo *)b)->name);
 }
 
 void error_exit(char *errstr) {
@@ -237,9 +239,6 @@ int main(int argc, char** argv) {
 
   if (argi >= argc) usage(argv[0]);
 
-  for (int i = 0; i < 256; i++)
-    instruction_names[i] = NULL;
-
   find = strrchr(argv[argi], '/');
   if (find == NULL)
     find = argv[argi];
@@ -315,16 +314,16 @@ int main(int argc, char** argv) {
     }
 
     for (i = 0; i < instruction_count; i++) {
-      if (!strcmp(instrs[i], name)) {
+      if (!strcmp(instruction[i].name, name)) {
         break;
       }
     }
-    if (i < instruction_count)
-      opcodes[opc_offset].instr_num = i;
-    else {
-      opcodes[opc_offset].instr_num = instruction_count;
-      instrs[instruction_count++] = StrDup(name);
+    if (i >= instruction_count) {
+      instruction[instruction_count].name = StrDup(name);
+      instruction[instruction_count].instr_num = instruction_count;
+      instruction_count++;
     }
+    opcodes[opc_offset].instr_num = instruction[i].instr_num;
     opcodes[opc_offset].abbrev = StrDup(name);
     opcode_count++;
   }
@@ -411,18 +410,7 @@ int main(int argc, char** argv) {
 
   // Sort instruction names alphabetically
   fprintf(stderr, "Sorting instructions alphabetically.\n");
-  qsort(&instrs[0], instruction_count, sizeof(char*), compar_str);
-  // Now update the instruction numbers in the array
-  for (int i = 0; i < MAX_OPCODES; i++) {
-    if (opcodes[i].defined && opcodes[i].abbrev != NULL) {
-      for (int j = 0; j < instruction_count; j++) {
-        if (!strcmp(opcodes[i].abbrev, instrs[j])) {
-          opcodes[i].instr_num = j;
-          break;
-        }
-      }
-    }
-  }
+  qsort(&instruction, instruction_count, sizeof(instinfo), compar_inst);
 
   /*
    * Now generate the instruction tables.
@@ -454,12 +442,11 @@ int main(int argc, char** argv) {
   for (int i = 0; i < instruction_count; i++) {
     // skip reserved 45GS02 instructions
     // if (!strcmp(instrs[i], "RESQ") || !strcmp(instrs[i], "RSVQ")) continue;
-    instruction = instrs[i];
     extra_cycles = 0;
     for (int ipf = 0; ipf < 7; ++ipf)
       strcpy(pflags[ipf], "$\\cdot$");
 
-    snprintf(filename, sizeof(filename), "instruction_sets/inst.%s", instrs[i]);
+    snprintf(filename, sizeof(filename), "instruction_sets/inst.%s", instruction[i].name);
     FILE* f = fopen(filename, "rb");
     if (!f) {
       fprintf(stderr, "WARNING: Could not read %s\n", filename);
@@ -523,17 +510,17 @@ int main(int argc, char** argv) {
       //      fprintf(stderr,"%s Long desc:\n%s\n\n\n\n",instruction,long_description);
       fclose(f);
     }
-    fprintf(stderr, "%s - %s:\n", instrs[i], short_description);
+    fprintf(stderr, "%s - %s:\n", instruction[i].name, short_description);
 
     is_unintended = strstr(short_description, "(unintended instruction)");
 
     if (is_unintended) {
       *is_unintended = 0;
-      fprintf(tf1, "\n\n\\subsection{\\textcolor{red}{%s [unintended]}}\n", instruction);
+      fprintf(tf1, "\n\n\\subsection{\\textcolor{red}{%s [unintended]}}\n", instruction[i].name);
     }
     else
-      fprintf(tf1, "\n\n\\subsection{%s}\n", instruction);
-    fprintf(tf1, "\\index{%s}%s\n\n\n", instruction, long_description);
+      fprintf(tf1, "\n\n\\subsection{%s}\n", instruction[i].name);
+    fprintf(tf1, "\\index{%s}%s\n\n\n", instruction[i].name, long_description);
 
     delmodify65ce02_note_seen = 0;
     delidle4510_note_seen = 0;
@@ -549,7 +536,7 @@ int main(int argc, char** argv) {
            " & \\multicolumn{10}{l}{%s} & \\\\\n"
            " & & & & {\\bf N} & {\\bf Z} & {\\bf I} & {\\bf C} & {\\bf D} & {\\bf V} & {\\bf E} & \\\\\n"
            " & & &",
-        instruction, short_description, processor, action);
+        instruction[i].name, short_description, processor, action);
 
     for (int ipf = 0; ipf < 7; ipf++)
       fprintf(tf1, " & {\\bf %s}", pflags[ipf]);
@@ -561,14 +548,14 @@ int main(int argc, char** argv) {
 
     int j;
     for (j = 0; j < MAX_OPCODES; j++) {
-      if (opcodes[j].instr_num == i) {
+      if (opcodes[j].instr_num == instruction[i].instr_num) {
         int m = opcodes[j].modeId;
         if (m < 0)
           m = 0;
         if (DEBUG) fprintf(stderr,"  %d %08X %d%d%d\n", j, opcodes[j].bytes, opcodes[j].isFar, opcodes[j].isQuad, opcodes[j].isFarQuad);
         opcodes[j].isUnintended = (is_unintended!=NULL) || (!strncmp(opcodes[j].abbrev, "NOP", 3) && j != 0xEA); // only EA is the real NOP
         addressing_mode = modeinfo[m].description ? modeinfo[m].description : "No description";
-        snprintf(assembly, 256, "%s %s", instruction, modeinfo[m].texmode);
+        snprintf(assembly, 256, "%s %s", instruction[i].name, modeinfo[m].texmode);
 
         if (opcodes[j].bytes & 0xff000000) {
           snprintf(opcode, 16, "%02X %02X %02X %02X", opcodes[j].bytes >> 24, (opcodes[j].bytes >> 16) & 0xff,
@@ -715,138 +702,6 @@ int main(int argc, char** argv) {
       fprintf(tf1, "\\end{tabular}\n\\end{center}\n\n");
     }
     fclose(tf1);
-  }
-
-  // skip old tables
-  if (0) {
-    snprintf(filename, 1024, "%s-opcodes.tex", processor);
-    FILE* tf = fopen(filename, "wb");
-    if (tf) {
-      fprintf(tf, "\\begin{tabular}{l|l|l|l|l|l|l|l|l|l|l|l|l|l|l|l|l|}\n");
-      fprintf(tf, "\\cline{2-17}\n");
-      fprintf(tf, "& \\$x0 & \\$x1 & \\$x2 & \\$x3 & \\$x4 & \\$x5 & \\$x6 & \\$x7 & \\$x8 & \\$x9 & \\$xA & \\$xB & \\$xC "
-                  "& \\$xD & \\$xE & \\$xF \\\\ \\hline\n");
-      for (int i = 0; i < 16; i++) {
-        fprintf(tf, "\\multicolumn{1}{|l|}{\\$%Xx} ", i);
-        for (int j = 0; j < 16; j++)
-          if (i * 16 + j < 256)
-            fprintf(tf, "& %s     ", instruction_names[i * 16 + j] ? instruction_names[i * 16 + j] : "??");
-          else
-            fprintf(tf, "& ");
-        fprintf(tf, "     \\\\ \\hline\n");
-      }
-      fprintf(tf, "\\end{tabular}\n");
-      fclose(tf);
-    }
-
-    snprintf(filename, 1024, "%s-cycles.tex", processor);
-    tf = fopen(filename, "wb");
-    if (tf) {
-      fprintf(tf, "\\begin{tabular}{l|l|l|l|l|l|l|l|l|l|l|l|l|l|l|l|l|}\n");
-      fprintf(tf, "\\cline{2-17}\n");
-      fprintf(tf, "& \\$x0 & \\$x1 & \\$x2 & \\$x3 & \\$x4 & \\$x5 & \\$x6 & \\$x7 & \\$x8 & \\$x9 & \\$xA & \\$xB & \\$xC "
-                  "& \\$xD & \\$xE & \\$xF \\\\ \\hline\n");
-      for (int i = 0; i < 16; i++) {
-        fprintf(tf, "\\multicolumn{1}{|l|}{\\$%Xx} ", i);
-        for (int j = 0; j < 16; j++) {
-          char cycle_count[1024];
-          char cycle_notes[1024] = "";
-
-          if (cycle_count_list[i * 16 + j]) {
-            cycle_count[0] = 0;
-            for (int k = 0; isdigit(cycle_count_list[i * 16 + j][k]); k++) {
-              {
-                cycle_count[k] = cycle_count_list[i * 16 + j][k];
-                cycle_count[k + 1] = 0;
-                strcpy(cycle_notes, &cycle_count_list[i * 16 + j][k + 1]);
-              }
-            }
-          }
-          else {
-            snprintf(cycle_count, 1024, "??");
-            cycle_notes[0] = 0;
-          }
-
-          int delmodify65ce02_note = 0;
-          int delidle4510_note = 0;
-          int branch_note = 0;
-          int page_note = 0;
-          int read_note = 0;
-          int single_cycle = 0;
-
-          if (strstr(cycle_notes, "d")) {
-            delmodify65ce02_note = 1;
-            delmodify65ce02_note_seen = 1;
-          }
-          if (strstr(cycle_notes, "m")) {
-            delidle4510_note = 1;
-            delidle4510_note_seen = 1;
-          }
-          if (strstr(cycle_notes, "b")) {
-            branch_note = 1;
-            branch_note_seen = 1;
-          }
-          if (strstr(cycle_notes, "p")) {
-            page_note = 1;
-            page_note_seen = 1;
-          }
-          if (strstr(cycle_notes, "r")) {
-            read_note = 1;
-            read_note_seen = 1;
-          }
-          if (!strcmp(cycle_count, "1")) {
-            single_cycle = 1;
-            single_cycle_seen = 1;
-          }
-
-          snprintf(cycle_notes, 1024, "$^{%s%s%s%s%s%s}$", branch_note ? "b" : "", delmodify65ce02_note ? "d" : "",
-              delidle4510_note ? "m" : "", page_note ? "p" : "", read_note ? "r" : "", single_cycle ? "s" : "");
-
-          fprintf(tf, "& %s%s     ", cycle_count, cycle_notes);
-        }
-        fprintf(tf, "     \\\\ \\hline\n");
-      }
-      if (branch_note_seen)
-        fprintf(
-            tf, " \\multicolumn{1}{r}{$b$} & \\multicolumn{16}{l}{Add one cycle if branch crosses a page boundary.} \\\\\n");
-      if (delmodify65ce02_note_seen)
-        fprintf(tf, " \\multicolumn{1}{r}{$d$} & \\multicolumn{16}{l}{Subtract one cycle when CPU is at 3.5MHz. } \\\\\n");
-      if (delidle4510_note_seen)
-        fprintf(tf, " \\multicolumn{1}{r}{$m$} & \\multicolumn{16}{l}{Subtract non-bus cycles when at 40MHz. } \\\\\n");
-      if (page_note_seen)
-        fprintf(tf,
-            " \\multicolumn{1}{r}{$p$} & \\multicolumn{16}{l}{Add one cycle if indexing crosses a page boundary.} \\\\\n");
-      if (read_note_seen)
-        fprintf(tf, " \\multicolumn{1}{r}{$r$} & \\multicolumn{16}{l}{Add one cycle if clock speed is at 40 MHz.} \\\\\n");
-      if (single_cycle_seen)
-        fprintf(tf, " \\multicolumn{1}{r}{$s$} & \\multicolumn{16}{l}{Instruction requires 2 cycles when CPU is run at 1MHz "
-                    "or 2MHz.} \\\\\n");
-
-      fprintf(tf, "\\end{tabular}\n");
-      fclose(tf);
-    }
-
-    // write matrix of addressing modes
-
-    snprintf(filename, 1024, "%s-modes.tex", processor);
-    tf = fopen(filename, "wb");
-    if (tf) {
-      fprintf(tf, "\\begin{tabular}{l|l|l|l|l|l|l|l|l|l|l|l|l|l|l|l|l|}\n");
-      fprintf(tf, "\\cline{2-17}\n");
-      fprintf(tf, "& \\$x0 & \\$x1 & \\$x2 & \\$x3 & \\$x4 & \\$x5 & \\$x6 & \\$x7 & \\$x8 & \\$x9 & \\$xA & \\$xB & \\$xC "
-                  "& \\$xD & \\$xE & \\$xF \\\\ \\hline\n");
-      for (int i = 0; i < 16; i++) {
-        fprintf(tf, "\\multicolumn{1}{|l|}{\\$%Xx} ", i);
-        for (int j = 0; j < 16; j++) {
-          int m = opcodes[i * 16 + j].modeId;
-          fprintf(tf, "& %s     ", m>=0?modeinfo[m].texmode:"???");
-        }
-        fprintf(tf, "     \\\\ \\hline\n");
-      }
-
-      fprintf(tf, "\\end{tabular}\n");
-      fclose(tf);
-    }
   }
 
   fprintf(stderr, "\n");
